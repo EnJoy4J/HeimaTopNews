bc92f4bafd959502d976099da434c43c2d636a0e
EnJoy <913505342@qq.com>
2021-08-08 20:00:23 +0800

    app端用户列表查询 +自媒体用户保存和按照名字查询自媒体用户 1111

--- heima-leadnews-apis/src/main/java/com/heima/apis/admin/WmUserControllerApi.java
+++ heima-leadnews-apis/src/main/java/com/heima/apis/admin/WmUserControllerApi.java
@@ -1,5 +1,6 @@
 package com.heima.apis.admin;
 
+import com.alibaba.nacos.client.utils.JSONUtils;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.media.pojos.WmUser;
 
@@ -9,15 +10,16 @@ import com.heima.model.media.pojos.WmUser;
 public interface WmUserControllerApi {
     /**
      * 保存自媒体用户
-     * @param Wmuser
+     * @param user
      * @return
      */
-    public ResponseResult save(WmUser Wmuser);
+    public ResponseResult save(WmUser user);
 
     /**
-     * 根据名称查找自媒体用户
+     * 根据名称查找自媒体用户124
      * @param name
      * @return
      */
     public WmUser findByName(String name);
+
 }
